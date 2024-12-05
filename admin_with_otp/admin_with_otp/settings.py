4f91396856f4845aed3d12e338ca45e25e35bf61
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-12-05 17:53:43 +0330

    creating a custom user model

--- admin_with_otp/admin_with_otp/settings.py
+++ admin_with_otp/admin_with_otp/settings.py
@@ -109,6 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTH_USER_MODEL = 'users.CustomUser'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
--- admin_with_otp/users/models.py
+++ admin_with_otp/users/models.py
@@ -1,3 +1,55 @@
+from multiprocessing.dummy import Value
 from django.db import models
+from django.contrib.auth.models import  AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils.translation import  gettext_lazy as _
+import json
+import uuid as uuid_
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, mobile, password, **other_fields):
+        
+        if not mobile:
+            raise ValueError(_('You need to provide a mobile number'))
+        if not password:
+            raise ValueError(_('You need to provide a password'))
+        if not isinstance(password, str):
+            raise ValueError(_('You need to provide a password'))
+        
+        
+        # email = self.normalize_email(email)
+        user =  self.model(mobile=mobile, **other_fields)
+        user.is_active = True
+        user.set_password(password)
+        user.save()
+        return user
+
+
+    def create_superuser(self, mobile, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_superuser', True)
+        return self.create_user(mobile, password, **other_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    
+    # common fields
+    mobile = models.CharField(unique=True, max_length=20)
+    first_name = models.CharField(unique=False, blank=True, max_length=150)
+    
+    # status fields
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'mobile' # ! important, changes the default username field for admin
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return self.mobile
+
+
+
+        
