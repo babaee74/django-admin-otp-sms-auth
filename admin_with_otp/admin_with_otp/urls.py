bf83044dcd91d789a46e850a657db5c17730caeb
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-12-05 17:32:18 +0330

    replacing new custom admin with old admin

--- admin_with_otp/admin_with_otp/urls.py
+++ admin_with_otp/admin_with_otp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from custom_admin.sites import admin_site
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
 ]
