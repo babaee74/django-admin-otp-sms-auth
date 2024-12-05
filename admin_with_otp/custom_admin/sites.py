cd499c19a1614377b0c6b8b986839e30efe2d3ef
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-12-05 17:26:53 +0330

    overriding the default login method to support OTP

--- admin_with_otp/custom_admin/sites.py
+++ admin_with_otp/custom_admin/sites.py
@@ -26,6 +26,38 @@ class AdminSite(admin.AdminSite):
         # ! and finds the first match.
         return custom_patterns + urls
     
+    def login(self, request, extra_context=None):
+        """
+        Use default login procedure for all methods other than POST (mainly GET).
+        """
+        if request.method == "POST":
+            mobile = request.POST.get("username")
+            password = request.POST.get("password")
+            # print("Admin Login Creds:", mobile, password)
 
+            if mobile is None or password is None:
+                messages.add_message(request, messages.ERROR, "Username or Password is empty!")
+            
+            user = authenticate(request=request, mobile=mobile, password=password)
+            if user is not None:
+                # TODO: implement SMS OTP
+                otp_code = get_otp_code(user.mobile)
+                
+                if otp_code is not None:
+                    # TODO: handle session vars
+                    # * We will create a session variable to store these:
+                    # ! OTP code, OTP code exp date, login state
+                    request.session["otp_attempts"] = 0
+                    request.session["login_state"] = user.pk
+                    request.session['otp_code'] = otp_code
+                    request.session['otp_exp'] = (timezone.now() + timedelta(minutes=OTP_DURATION)).isoformat()
+                    return redirect("/admin/2fa-otp/")
+                
+                else:
+                    messages.add_message(request, messages.ERROR, "Something went wrong, please report this problem.")
 
-
+                
+            else:
+                messages.add_message(request, messages.ERROR, "Wrong Username or Password!")
+        
+        return super().login(request, extra_context=extra_context)
