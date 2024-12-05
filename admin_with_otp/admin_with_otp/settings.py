c72b58c9d3291db64a1b47aa730740e23a33cb99
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-12-05 17:58:32 +0330

    adding templates dir to the settings

--- admin_with_otp/admin_with_otp/settings.py
+++ admin_with_otp/admin_with_otp/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'admin_with_otp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
