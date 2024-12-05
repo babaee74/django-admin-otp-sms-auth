41f031d6235576e1ee27c7e36c4d8fc78563ad1e
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-12-05 17:27:18 +0330

    added otp verify and cancel view

--- admin_with_otp/custom_admin/sites.py
+++ admin_with_otp/custom_admin/sites.py
@@ -61,3 +61,74 @@ class AdminSite(admin.AdminSite):
                 messages.add_message(request, messages.ERROR, "Wrong Username or Password!")
         
         return super().login(request, extra_context=extra_context)
+    
+    def verify_otp(self, request):
+        if request.method == "POST":
+            input_otp = request.POST.get('otp')
+
+            session_otp_code = request.session.get('otp_code', None)
+            otp_exp_date_str = request.session.get('otp_exp', None) # ISO format
+            otp_attempts = request.session.get('otp_attempts', -1)
+
+            if otp_attempts>MAX_OTP_TRIES:
+                messages.add_message(request, messages.ERROR, "Too many attempts, try again later")
+                request.session.flush()
+                return redirect('admin:login')
+
+            if session_otp_code is None or otp_exp_date_str is None or otp_attempts<0:
+                messages.add_message(request, messages.ERROR, "Verification code expired, try Again")
+                request.session.flush()
+                return redirect('admin:login')
+
+            # ISO to datetime
+            otp_exp_date = timezone.datetime.fromisoformat(otp_exp_date_str)
+            
+            if timezone.now() > otp_exp_date:
+                messages.add_message(request, messages.ERROR, "Verification code expired, try Again")
+                request.session.flush()
+                return redirect('admin:login')
+
+            if input_otp==session_otp_code:
+                # ! We saved user.pk in login state, 
+                # ! we can retrieve the user record using this pk
+                user_pk = request.session.get('login_state', None)
+                
+                if user_pk is not None:
+                    user = get_user_model().objects.get(pk=user_pk)
+                    
+                    # sets the request.user object for next requests
+                    auth_login(request, user) 
+                    
+                    request.session.pop('otp_attempts', None)
+                    request.session.pop('login_state', None)
+                    request.session.pop('otp_code', None)
+                    request.session.pop('otp_exp', None)
+                    
+                    return redirect('admin:index') # Enjoy the Django Dashboard
+                else:
+                    # ! It will return to otp page
+                    messages.add_message(request, messages.ERROR, "Verification code expired, try Again")
+            else:
+                # ! It will return to otp page
+                request.session["otp_attempts"] = otp_attempts + 1
+                messages.error(request, "Invalid Verification Code. Try Again")
+        
+        
+        # ? show OTP Page (GET) to user, 
+        # ? use the template inside templates dir (its path setup inside settings).
+        return render(request, 'admin/2fa_verification.html')
+
+
+
+    def cancel_otp(self, request):
+        # request.session.pop('login_state', None)
+        # request.session.pop('otp_code', None)
+        # request.session.pop('otp_exp', None)
+        request.session.flush()
+        return redirect("admin:login")
+
+
+admin_site = AdminSite(name='custom_admin')
+
+
+
