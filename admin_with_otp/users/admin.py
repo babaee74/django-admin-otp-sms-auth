8b8db9e8bf3d96d5baa73c8f3d60d5a4d82e5b00
babaee74 <60437337+babaee74@users.noreply.github.com>
2024-12-05 17:54:02 +0330

    custom admin forms

--- admin_with_otp/users/admin.py
+++ admin_with_otp/users/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import  UserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import AdminPasswordChangeForm
+from custom_admin.sites import admin_site
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+    search_fields = ('mobile',)
+    ordering = ('mobile',)
+    list_display = ('mobile', 'is_active', 'is_staff',)
+    list_display_links = ("mobile",)
+
+    fieldsets = (
+        (None, {'fields':('mobile',)}),
+        ('Permissions',{'fields':('is_staff', 'is_active',)}),
+        ('Password', {'fields': ('password',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ('mobile','password1', 'password2', )}
+        ),
+    )
+    
+
+
+# Now register the new UserAdmin...
+# admin.site.register(CustomUser, UserAdminConfig)
+admin_site.register(CustomUser, UserAdminConfig) #! custom admin instance
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
---
+++ admin_with_otp/users/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth import password_validation
+from .models import CustomUser
+
+
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'password_mismatch':  ('The two password fields didn’t match.'),
+    }
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label= ("Password confirmation"),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        strip=False,
+        help_text= ("Enter the same password as before, for verification."),
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('mobile',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._meta.model.USERNAME_FIELD in self.fields:
+            self.fields[self._meta.model.USERNAME_FIELD].widget.attrs['autofocus'] = True
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        # Validate the password after self.instance is updated with form data
+        # by super().
+        password = self.cleaned_data.get('password2')
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except ValidationError as error:
+                self.add_error('password2', error)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+    # password = ReadOnlyPasswordHashField()
+
+    password = ReadOnlyPasswordHashField(
+        label=("Password"),
+        help_text= (
+            'Raw passwords are not stored, so there is no way to see this '
+            'user’s password, but you can change the password using '
+            '<a href="{}">this form</a>.'
+        ),
+    )
+
+    class Meta:
+        model = CustomUser
+        fields =  ('mobile', 'first_name',)
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        password = self.fields.get('password')
+        if password:
+            password.help_text = password.help_text.format('../password/')
+        user_permissions = self.fields.get('user_permissions')
+        if user_permissions:
+            user_permissions.queryset = user_permissions.queryset.select_related('content_type')
